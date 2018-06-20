f584037c41d65ab1d819ccdb24c45d85853e64f1
Kim Seungha <seungha.me@gmail.com>
2018-06-20 16:33:44 +0900

    LoginForm PC 제작

---
+++ src/components/LoginForm.stories.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import LoginForm from './LoginForm';
+
+storiesOf('LoginForm', module)
+  .add('empty', () => (
+    <LoginForm />
+  ))
+  .add('with texts', () => (
+    <LoginForm username="fds" password="fds" />
+  ))
+  .add('with actions', () => (
+    <LoginForm
+      onUsernameChange={action('onUsernameChange')}
+      onPasswordChange={action('onPasswordChange')}
+      onSubmit={action('onSubmit')}
+    />
+  ))
